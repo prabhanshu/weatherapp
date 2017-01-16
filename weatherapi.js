a264f08c2a6b206ea3da4bab348e39af0d3f85ca
Prabhanshu Pandit <prabhanshupandit.pu@gmail.com>
2017-01-16 12:54:46 +0530

    first commit

---
+++ weatherapi.js
@@ -0,0 +1,28 @@
+var app = angular.module('myapp', []);
+app.controller('DemoCtrl', function($http,$scope) {
+    var ctrl = this;
+    var URL = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+    //var cityName = document.getElementById("search_box").value;
+    $scope.request = {
+        method: 'GET',
+        url: URL,
+        params: {
+            q: 'london',
+            mode: 'json',
+            units: 'metric',
+            // cnt: '7',
+            appid: '666edb75be8f5bcf90b099fb27b6bbb3'
+        }
+    }
+function test() {
+    $http($scope.request)
+        .then(function (response) {
+            ctrl.data = response.data;
+            console.log(ctrl.data);
+        })
+}
+test();
+    $scope.$watch('request.params.q',function () {
+        test();
+    })
+})
